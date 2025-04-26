67cd8a735a4b219556f523e7d9ffd507a310a4e6
shahnawazAlam3641 <shahnawazalam3641@gmail.com>
2025-04-26 18:49:53 +0530

    update urls

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:3001/api/v1";
+export const BASE_URL = process.env.VITE_BASE_URL;
+export const SOCKET_URL = process.env.VITE_SOCKET_URL;
--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+export const socket = io(process.env.VITE_SOCKET_URL, {
   withCredentials: true,
   autoConnect: false,
 });
